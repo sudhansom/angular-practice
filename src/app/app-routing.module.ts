4e3c66e2a329fb01aa1b2c00d2186044b04b7b3b
bhuwan <bkspoudel@gmail.com>
2023-08-23 19:46:33 +0200

    create an about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', loadChildren: ()=>import('./pages/home/homponent.module').then((mod)=>mod.HomepageModule)},
+  {path: 'about', loadChildren: ()=>import('./pages/about/about.module').then((mod)=>mod.AboutModule)},
 ];
 
 @NgModule({
