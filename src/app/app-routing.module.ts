423c53b2a4a58e4e576f70cc8c6b1342b5767520
bhuwan <bkspoudel@gmail.com>
2023-08-23 20:08:49 +0200

    create a page to practice observable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: ()=>import('./pages/home/homponent.module').then((mod)=>mod.HomepageModule)},
-  {path: 'about', loadChildren: ()=>import('./pages/about/about.module').then((mod)=>mod.AboutModule)},
-  {path: 'array-methods', loadChildren: ()=>import('./pages/array-methods/array-methods.module').then((mod)=>mod.ArrayMethodsModule)},
+  { path: '', loadChildren: ()=>import('./pages/home/homponent.module').then((mod)=>mod.HomepageModule)},
+  { path: 'about', loadChildren: ()=>import('./pages/about/about.module').then((mod)=>mod.AboutModule)},
+  { path: 'array-methods', loadChildren: ()=>import('./pages/array-methods/array-methods.module').then((mod)=>mod.ArrayMethodsModule)},
+  { path: 'observable', loadChildren: ()=>import('./pages/observable/observable.module').then((mod)=>mod.ObservableModule)},
 ];
 
 @NgModule({
---
+++ src/app/pages/observable/observable.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-observable',
+  templateUrl: './observable.component.html',
+  styleUrls: ['./observable.component.scss'],
+  //changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ObservableComponent  {
+
+}
