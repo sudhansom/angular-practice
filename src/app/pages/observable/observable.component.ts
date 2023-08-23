5738b7ae8312b8a1e9a286711265a33a3ff512c4
bhuwan <bkspoudel@gmail.com>
2023-08-23 20:22:32 +0200

    create custom observable and unsubscribe on Destroy

--- src/app/pages/observable/observable.component.ts
+++ src/app/pages/observable/observable.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 
 
@@ -8,6 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./observable.component.scss'],
   //changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ObservableComponent  {
+export class ObservableComponent implements OnInit, OnDestroy  {
+  subscription!: Subscription;
 
+  ngOnInit(): void {
+    const obs = new Observable(observer => {
+      let count = 0;
+      setInterval(()=>{
+        count ++;
+        observer.next(count);
+      }, 1000);
+    });
+    this.subscription = obs.subscribe(data => console.log(data));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
