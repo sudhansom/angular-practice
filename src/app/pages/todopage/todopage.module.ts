cf49729ae099e0a17b76da410d24a2a68748b9a7
bhuwan <bkspoudel@gmail.com>
2023-08-24 13:10:09 +0200

    create few components and add it to todopage

--- src/app/pages/todopage/todopage.module.ts
+++ src/app/pages/todopage/todopage.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { TodopageComponent } from "./todopage.component";
+import { EachTaskComponent } from "src/app/components/each-task/each-task.component";
+import { HeaderComponent } from "src/app/components/header/header.component";
+import { TasksComponent } from "src/app/components/tasks/tasks.component";
+
 
 const appRoutes: Routes = [
   {
@@ -14,7 +18,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     TodopageComponent,
-
+    EachTaskComponent,
+    HeaderComponent,
+    TasksComponent
   ],
   imports: [
     CommonModule,
