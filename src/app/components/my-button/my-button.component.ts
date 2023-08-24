faa01c65c8b72060f1790e63fe2019bd2c590074
bhuwan <bkspoudel@gmail.com>
2023-08-24 13:42:05 +0200

    fix styling to the button

--- src/app/components/my-button/my-button.component.ts
+++ src/app/components/my-button/my-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-my-button',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-button.component.scss']
 })
 export class MyButtonComponent {
-
+  @Input() backgroundColor = 'green';
+  @Input() buttonText = 'Show Form';
 }
--- src/app/components/tasks/tasks.module.ts
+++ src/app/components/tasks/tasks.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 
 import { TasksComponent } from "./tasks.component";
+import { EachTaskModule } from "../each-task/each-task.module";
 
 @NgModule({
   declarations:[TasksComponent],
-  imports:[],
+  imports:[EachTaskModule],
   exports:[TasksComponent],
   providers:[]
 })
