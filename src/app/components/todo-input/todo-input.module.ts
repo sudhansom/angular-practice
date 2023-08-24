633be05eabd00159c70727a8d77b9b90e3500cd8
bhuwan <bkspoudel@gmail.com>
2023-08-24 14:07:38 +0200

    fix todo-input in todo page below header

---
+++ src/app/components/todo-input/todo-input.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+
+import { TodoInputComponent } from "./todo-input.component";
+
+@NgModule({
+  declarations:[TodoInputComponent],
+  imports:[],
+  providers:[],
+  exports:[TodoInputComponent]
+})
+
+export class TodoInputModule {
+
+}
--- src/app/pages/todopage/todopage.module.ts
+++ src/app/pages/todopage/todopage.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { TodopageComponent } from "./todopage.component";
 import { HeaderModule } from "src/app/components/header/header.module";
 import { TasksModule } from "src/app/components/tasks/tasks.module";
+import { TodoInputModule } from "src/app/components/todo-input/todo-input.module";
 
 const appRoutes: Routes = [
   {
@@ -22,6 +23,7 @@ const appRoutes: Routes = [
     RouterModule.forChild(appRoutes),
     HeaderModule,
     TasksModule,
+    TodoInputModule,
   ],
   exports:[],
   providers: [],
