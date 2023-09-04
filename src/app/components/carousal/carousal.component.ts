53701f6752c866cf7dc754e922c2de3719f6430b
bhuwan <bkspoudel@gmail.com>
2023-09-04 15:34:55 +0200

    fix next and prev button in a better way

--- src/app/components/carousal/carousal.component.ts
+++ src/app/components/carousal/carousal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BehaviorSubject, combineLatest, map } from 'rxjs';
 
 
@@ -7,13 +7,13 @@ import { BehaviorSubject, combineLatest, map } from 'rxjs';
   selector: 'app-carousal',
   templateUrl: './carousal.component.html',
   styleUrls: ['./carousal.component.scss'],
-  //changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CarousalComponent  {
   currentIndex$ = new BehaviorSubject(0);
   noOfSlides$ = new BehaviorSubject(6);
-  showPrev$ = new BehaviorSubject(false);
-  showNext$ = new BehaviorSubject(true);
+  // showPrev$ = new BehaviorSubject(false);
+  // showNext$ = new BehaviorSubject(true);
 
   slideToIndex(i: number){
     this.currentIndex$.next(i);
@@ -25,21 +25,37 @@ export class CarousalComponent  {
       })
      }
   ))
+  showPrev$ = this.currentIndex$.pipe(
+    map(currentIndex => {
+      if(currentIndex === 0){
+        return false
+      }
+      return true;
+    })
+  )
 
+  showNext$ = combineLatest([this.currentIndex$, this.noOfSlides$]).pipe(
+    map(([currentIndex,noOfSlides]) => {
+      if(currentIndex < noOfSlides - 1){
+        return true;
+      }
+      return false;
+    }
+  ))
 
   handleNext(){
     if(this.currentIndex$.getValue() >= 0 && this.currentIndex$.getValue() < this.noOfSlides$.getValue()-1){
       this.currentIndex$.next(this.currentIndex$.getValue() + 1);
     }
-    this.showPrev$.next(this.currentIndex$.getValue()>=1?true:false);
-    this.showNext$.next(this.currentIndex$.getValue()<(this.noOfSlides$.getValue()-1)?true:false);
+    // this.showPrev$.next(this.currentIndex$.getValue()>=1?true:false);
+    // this.showNext$.next(this.currentIndex$.getValue()<(this.noOfSlides$.getValue()-1)?true:false);
   }
 
   handlePrev(){
     if(this.currentIndex$.getValue() > 0 && this.currentIndex$.getValue() <= this.noOfSlides$.getValue()){
       this.currentIndex$.next(this.currentIndex$.getValue() - 1);
     }
-    this.showPrev$.next(this.currentIndex$.getValue()>=1?true:false);
-    this.showNext$.next(this.currentIndex$.getValue()<(this.noOfSlides$.getValue()-1)?true:false);
+    // this.showPrev$.next(this.currentIndex$.getValue()>=1?true:false);
+    // this.showNext$.next(this.currentIndex$.getValue()<(this.noOfSlides$.getValue()-1)?true:false);
   }
 }
