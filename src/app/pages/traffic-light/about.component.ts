dc0fcdee1cff79022e52c46e70993078349ce221
bhuwan <bkspoudel@gmail.com>
2023-09-04 18:36:31 +0200

    fix trafic light

--- src/app/pages/traffic-light/about.component.ts
+++ src/app/pages/traffic-light/about.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 
 
@@ -6,8 +7,45 @@ import { Component } from '@angular/core';
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
-  //changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutComponent  {
+export class AboutComponent implements OnInit {
+  lightColorRed$ = new BehaviorSubject('red');
+  lightColorYellow$ = new BehaviorSubject('');
+  lightColorGreen$ = new BehaviorSubject('');
 
+
+  timerId : any;
+  ngOnInit(): void {
+       setInterval(()=>{
+        this.operate();
+       }, 10000);
+  }
+  operate(){
+    clearTimeout(this.timerId);
+    this.timerId = setTimeout(()=>{
+      this.lightColorGreen$.next('');
+      this.lightColorRed$.next('red');
+      this.lightColorYellow$.next('yellow');
+      clearTimeout(this.timerId);
+    }, 2000)
+    this.timerId = setTimeout(()=>{
+      this.lightColorGreen$.next('green');
+      this.lightColorRed$.next('');
+      this.lightColorYellow$.next('');
+      clearTimeout(this.timerId);
+    }, 8000)
+    this.timerId = setTimeout(()=>{
+      this.lightColorGreen$.next('');
+      this.lightColorRed$.next('');
+      this.lightColorYellow$.next('yellow');
+      clearTimeout(this.timerId);
+    }, 10000)
+    this.timerId = setTimeout(()=>{
+      this.lightColorGreen$.next('');
+      this.lightColorRed$.next('red');
+      this.lightColorYellow$.next('');
+      clearTimeout(this.timerId);
+    }, 12000)
+  }
 }
