b8ffe41339fe30ebf2b696e5f1b9fd12282687c0
bhuwan <bkspoudel@gmail.com>
2023-09-14 21:46:21 +0200

    add new component to practice navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,57 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
+
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent  {
+  currentIndex$ = new BehaviorSubject(0);
+  noOfSlides$ = new BehaviorSubject(6);
+  // showPrev$ = new BehaviorSubject(false);
+  // showNext$ = new BehaviorSubject(true);
+
+  slideToIndex(i: number){
+    this.currentIndex$.next(i);
+  }
+  bullets$ = combineLatest([this.currentIndex$, this.noOfSlides$]).pipe(
+    map(([currentIndex, noOfSlides])=> {
+      return Array.from(Array(noOfSlides)).map((v,i)=>{
+        return i === currentIndex;
+      })
+     }
+  ))
+  showPrev$ = this.currentIndex$.pipe(
+    map(currentIndex => {
+      if(currentIndex === 0){
+        return false
+      }
+      return true;
+    })
+  )
+
+  showNext$ = combineLatest([this.currentIndex$, this.noOfSlides$]).pipe(
+    map(([currentIndex,noOfSlides]) => {
+      if(currentIndex < noOfSlides - 1){
+        return true;
+      }
+      return false;
+    }
+  ))
+
+  handleNext(){
+    if(this.currentIndex$.getValue() >= 0 && this.currentIndex$.getValue() < this.noOfSlides$.getValue()-1){
+      this.currentIndex$.next(this.currentIndex$.getValue() + 1);
+    }
+  }
+
+  handlePrev(){
+    if(this.currentIndex$.getValue() > 0 && this.currentIndex$.getValue() <= this.noOfSlides$.getValue()){
+      this.currentIndex$.next(this.currentIndex$.getValue() - 1);
+    }
+  }
+}
