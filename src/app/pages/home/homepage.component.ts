07daa17aba777da6d2e85ef2180f5392321a773c
bhuwan <bkspoudel@gmail.com>
2023-09-18 20:12:23 +0200

    create a custom observer

--- src/app/pages/home/homepage.component.ts
+++ src/app/pages/home/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
 
 
 
@@ -44,6 +45,30 @@ export class HomepageComponent implements OnInit  {
   ]
   newArray = []
   ngOnInit(): void {
+
+    const customIntervalObserver = Observable.create((observer) => {
+      let count = 0;
+      setInterval(()=>{
+        console.log(count);
+        observer.next(count);
+        if(count > 5){
+          observer.error("error occured....");
+        }
+        count ++;
+      }, 1000)
+    });
+    customIntervalObserver.subscribe((data)=>{
+      console.log('dfdsfsd',data);
+    },
+    (error)=>{
+      console.log(error);
+    },
+    ()=>{
+      console.log("completed....");
+    }
+
+
+    )
     //debugger;
     this.input.forEach(
       item => {
