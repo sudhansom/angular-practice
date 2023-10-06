4eb7031d75b4df52024eb6682d294702a86449cc
bhuwan <bkspoudel@gmail.com>
2023-10-06 18:10:20 +0200

    delete observables

--- src/app/pages/observable/observable.component.ts
+++ src/app/pages/observable/observable.component.ts
@@ -1,11 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, shareReplay } from 'rxjs';
-import { Subscription, map, filter, tap } from 'rxjs';
-
-import { createObservables } from 'src/app/util/createObservable';
-import { Course } from 'src/app/util/type';
-
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-observable',
@@ -13,26 +6,6 @@ import { Course } from 'src/app/util/type';
   styleUrls: ['./observable.component.scss'],
   //changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ObservableComponent implements OnInit  {
-  subscription!: Subscription;
-  beginner$: Observable<Course[]>;
-  advanced$: Observable<Course[]>;
-
-  ngOnInit(): void {
-    const http$: Observable<Course[]> = createObservables('http://localhost:9001/courses');
-    const courses$ = http$.pipe(
-      map(res => res),
-      tap((val)=>console.log('take the value here... ', val)),
-      shareReplay()
-      );
-
-
-   this.beginner$ = courses$.pipe(
-                              map(courses => courses
-                                .filter(course => course.category==='beginner')));
-   this.advanced$ = courses$.pipe(
-                              map(courses => courses
-                                .filter(course => course.category==='advance')));
-
-  }
+export class ObservableComponent implements OnInit {
+  ngOnInit(): void {}
 }
