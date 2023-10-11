631dda350f99f380fb267b3dfd302e5310aad549
bhuwan <bkspoudel@gmail.com>
2023-10-11 23:01:41 +0200

    add a prime number check

--- src/app/pages/interview-prep/about.component.ts
+++ src/app/pages/interview-prep/about.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
-  //changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutComponent implements OnInit {
   isPalindrome$ = new BehaviorSubject<boolean>(true);
@@ -25,5 +25,24 @@ export class AboutComponent implements OnInit {
       left++;
       right--;
     }
+  }
+
+  isPrimeNumber(num: number) {
+    console.log(num % 3);
+    if (num <= 1) {
+      return false;
+    }
+    if (num <= 3) {
+      return true;
+    }
+    if (num % 2 === 0 || num % 3 === 0) {
+      return false;
+    }
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) {
+        return false;
+      }
+    }
+    return true;
   }
 }
