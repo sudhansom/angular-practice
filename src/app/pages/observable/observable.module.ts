7d254886c7aecf830f18256942fcccae53420298
bhuwan <bkspoudel@gmail.com>
2023-11-29 22:09:35 +0100

    practice of and from

--- src/app/pages/observable/observable.module.ts
+++ src/app/pages/observable/observable.module.ts
@@ -1,27 +1,35 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { ObservableComponent } from "./observable.component";
+import { ObservableComponent } from './observable.component';
+import { from, of, map, mergeAll, tap, concatMap, concat } from 'rxjs';
 
 const appRoutes: Routes = [
   {
     path: '',
-    component: ObservableComponent
-  }
-]
+    component: ObservableComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    ObservableComponent,
-
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(appRoutes),
-  ],
-  exports:[],
+  declarations: [ObservableComponent],
+  imports: [CommonModule, RouterModule.forChild(appRoutes)],
+  exports: [],
   providers: [],
   bootstrap: [],
 })
-export class ObservableModule {}
+export class ObservableModule {
+  getVideo(channel: string) {
+    return of(channel + ' Video uploaded');
+  }
+  //source1$ = of('Tech', 'News', 'Sports');
+  source1$ = from(['Tech', 'News', 'Sports']);
+
+  broadCast$ = this.source1$
+    .pipe(
+      map((channel) => this.getVideo(channel)),
+      mergeAll()
+    )
+    .subscribe(console.log);
+}
