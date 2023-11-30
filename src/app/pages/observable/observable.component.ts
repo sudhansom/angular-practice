56a1763a2c1c6be5ff15afecd86a2501d0744158
bhuwan <bkspoudel@gmail.com>
2023-11-30 10:44:41 +0100

    practice debounceTime and distinctUntilChanged

--- src/app/pages/observable/observable.component.ts
+++ src/app/pages/observable/observable.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, shareReplay } from 'rxjs';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  fromEvent,
+  shareReplay,
+} from 'rxjs';
 import { Subscription, map, filter, tap } from 'rxjs';
 
 import { createObservables } from 'src/app/util/createObservable';
 import { Course } from 'src/app/util/type';
-
-
 
 @Component({
   selector: 'app-observable',
@@ -13,26 +24,42 @@ import { Course } from 'src/app/util/type';
   styleUrls: ['./observable.component.scss'],
   //changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ObservableComponent implements OnInit  {
+export class ObservableComponent implements OnInit, AfterViewInit {
   subscription!: Subscription;
   beginner$: Observable<Course[]>;
   advanced$: Observable<Course[]>;
 
+  @ViewChild('input') input: ElementRef;
+
   ngOnInit(): void {
-    const http$: Observable<Course[]> = createObservables('http://localhost:9001/courses');
+    const http$: Observable<Course[]> = createObservables(
+      'http://localhost:9001/courses'
+    );
     const courses$ = http$.pipe(
-      map(res => res),
-      tap((val)=>console.log('take the value here... ', val)),
+      map((res) => res),
+      tap((val) => console.log('take the value here... ', val)),
       shareReplay()
-      );
+    );
 
+    this.beginner$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === 'beginner')
+      )
+    );
+    this.advanced$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === 'advance')
+      )
+    );
+  }
 
-   this.beginner$ = courses$.pipe(
-                              map(courses => courses
-                                .filter(course => course.category==='beginner')));
-   this.advanced$ = courses$.pipe(
-                              map(courses => courses
-                                .filter(course => course.category==='advance')));
-
+  ngAfterViewInit(): void {
+    fromEvent<any>(this.input.nativeElement, 'keyup')
+      .pipe(
+        map((event) => event.target.value),
+        debounceTime(200),
+        distinctUntilChanged()
+      )
+      .subscribe(console.log);
   }
 }
--- src/app/pages/observable/observable.module.ts
+++ src/app/pages/observable/observable.module.ts
@@ -1,9 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { AfterViewInit, ElementRef, NgModule, ViewChild } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ObservableComponent } from './observable.component';
-import { from, of, map, mergeAll, tap, concatMap, concat } from 'rxjs';
 
 const appRoutes: Routes = [
   {
@@ -19,17 +18,4 @@ const appRoutes: Routes = [
   providers: [],
   bootstrap: [],
 })
-export class ObservableModule {
-  getVideo(channel: string) {
-    return of(channel + ' Video uploaded');
-  }
-  //source1$ = of('Tech', 'News', 'Sports');
-  source1$ = from(['Tech', 'News', 'Sports']);
-
-  broadCast$ = this.source1$
-    .pipe(
-      map((channel) => this.getVideo(channel)),
-      mergeAll()
-    )
-    .subscribe(console.log);
-}
+export class ObservableModule {}
