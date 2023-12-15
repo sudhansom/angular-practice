76d3e184911586fe068c6786951a0902b5b2f0b5
bhuwan <bkspoudel@gmail.com>
2023-12-15 19:01:52 +0100

    create Student class and few students

---
+++ src/app/pages/students/models/student.ts
@@ -0,0 +1,13 @@
+export class Student {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+
+  constructor(id: number, name: string, username: string, password: string){
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.password = password;
+  }
+}
--- src/app/pages/students/services/student.service.ts
+++ src/app/pages/students/services/student.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from "@angular/core";
 
+import { Student } from "../models/student";
+
 @Injectable({
   providedIn: 'root',
 })
 export class StudentService {
-
+students: Student[] = [
+  new Student(1, 'Bal Krishna', 'bk', '123456'),
+  new Student(1, 'Bhuwan Krishna', 'bk', '123456'),
+  new Student(1, 'Resham Krishna', 'rk', '123456'),
+  new Student(1, 'Birendra Kumar', 'bk', '123456'),
+]
 }
