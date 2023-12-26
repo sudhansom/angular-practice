6cb1cda303ea5db5a6a0b01743883624e342440e
bhuwan <bkspoudel@gmail.com>
2023-12-26 22:05:06 +0100

    clean comments

--- src/app/pages/students/create-student/create-student.component.ts
+++ src/app/pages/students/create-student/create-student.component.ts
@@ -2,18 +2,18 @@ import { Component , inject, EventEmitter, Output} from '@angular/core';
 import { StudentService } from '../services/student.service';
 
 import { Student } from '../models/student';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
   selector: 'app-create-student',
   templateUrl: './create-student.component.html',
   styleUrls: ['./create-student.component.scss'],
-  //changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateStudentComponent {
   @Output() onCreate = new EventEmitter();
   dataService = inject(StudentService);
-  submitForm(form: any){
+  submitForm(form: NgForm){
     this.dataService.students.push(new Student(form.value.id, form.value.name, form.value.userName, form.value.password));
     form.reset();
     this.onCreate.emit();
