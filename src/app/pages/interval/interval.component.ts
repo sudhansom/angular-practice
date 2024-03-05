df7cbecbf10e7d8cd1ccde4af9c04508d32ee76e
bhuwan <bkspoudel@gmail.com>
2024-03-05 11:46:06 +0100

    apply onPUsh

--- src/app/pages/interval/interval.component.ts
+++ src/app/pages/interval/interval.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-interval',
   templateUrl: './interval.component.html',
   styleUrls: ['./interval.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IntervalComponent implements OnInit {
   myInterval$ = interval(2000);
+  val = 0;
 
   ngOnInit(): void {
-    this.myInterval$.subscribe((value) => console.log(value));
+    this.myInterval$.subscribe((value) => {
+      this.val = value;
+    });
   }
 }
