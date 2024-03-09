1288ee1086514e481d42c608d36f7871c3289928
bhuwan <bkspoudel@gmail.com>
2024-03-09 07:52:47 +0100

    style template-outlet page

--- src/app/pages/template-outlet/template-outlet.component.ts
+++ src/app/pages/template-outlet/template-outlet.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-template-outlet',
   templateUrl: './template-outlet.component.html',
-  styleUrls: ['./template-outlet.component.scss']
+  styleUrls: ['./template-outlet.component.scss'],
 })
-export class TemplateOutletComponent {
-
-}
+export class TemplateOutletComponent {}
